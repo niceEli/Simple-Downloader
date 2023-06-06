7e7626081e4f000db8453836beb1fb44d03e4c2b
Eli <78963198+niceEli@users.noreply.github.com>
2023-06-06 16:53:06 -0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -2,106 +2,140 @@ using System;
 using System.IO;
 using System.Net.Http;
 using System.Threading.Tasks;
+using System.IO.Compression;
 
 class Program
 {
-	static async Task Main (string[] args)
-	{
-		if (args.Length < 1)
-		{
-			Console.WriteLine("Usage: Simple-Downloader <url> [<directory>]");
-			return;
-		}
+    static async Task Main(string[] args)
+    {
+        if (args.Length < 1)
+        {
+            Console.WriteLine("Usage: Simple-Downloader <url> [<directory>] [-E or --Extract]");
+            return;
+        }
 
-		string source = args[0];
-		string destination = args.Length > 1 ? args[1] : "";
+        string source = args[0];
+        string destination = args.Length > 1 ? args[1] : "";
+        bool shouldExtract = args.Length > 2 && (args[2] == "-E" || args[2] == "--Extract");
 
-		try
-		{
-			if (IsUrl(source))
-			{
-				await DownloadFileAsync(source, destination);
-			}
-			else
-			{
-				if (!File.Exists(source))
-				{
-					Console.WriteLine("The source file does not exist.");
-					return;
-				}
+        try
+        {
+            if (IsUrl(source))
+            {
+                await DownloadFileAsync(source, destination);
+            }
+            else
+            {
+                if (!File.Exists(source))
+                {
+                    Console.WriteLine("The source file does not exist.");
+                    return;
+                }
 
-				CopyFile(source, destination);
-			}
-		}
-		catch (Exception ex)
-		{
-			Console.WriteLine($"An error occurred: {ex.Message}");
-		}
-	}
+                if (shouldExtract && IsZipFile(source))
+                {
+                    ExtractZipFile(source, destination);
+                }
+                else
+                {
+                    CopyFile(source, destination);
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"An error occurred: {ex.Message}");
+        }
+    }
 
-	static bool IsUrl (string path)
-	{
-		return Uri.TryCreate(path, UriKind.Absolute, out _);
-	}
+    static bool IsUrl(string path)
+    {
+        return Uri.TryCreate(path, UriKind.Absolute, out _);
+    }
 
-	static async Task DownloadFileAsync (string url, string destination)
-	{
-		using (HttpClient client = new HttpClient())
-		{
-			Uri uri = new Uri(url);
-			string fileName = Path.GetFileName(uri.LocalPath);
-			string filePath = string.IsNullOrEmpty(destination)
-				? Path.Combine(Environment.CurrentDirectory, fileName)
-				: Path.Combine(destination, fileName);
+    static bool IsZipFile(string path)
+    {
+        string extension = Path.GetExtension(path);
+        return string.Equals(extension, ".zip", StringComparison.OrdinalIgnoreCase);
+    }
 
-			using (var response = await client.GetAsync(url, HttpCompletionOption.ResponseHeadersRead))
-			{
-				response.EnsureSuccessStatusCode();
+    static async Task DownloadFileAsync(string url, string destination)
+    {
+        using (HttpClient client = new HttpClient())
+        {
+            Uri uri = new Uri(url);
+            string fileName = Path.GetFileName(uri.LocalPath);
+            string filePath = string.IsNullOrEmpty(destination)
+                ? Path.Combine(Environment.CurrentDirectory, fileName)
+                : Path.Combine(destination, fileName);
 
-				using (var stream = await response.Content.ReadAsStreamAsync())
-				using (var fileStream = new FileStream(filePath, FileMode.Create, FileAccess.Write))
-				{
-					const int bufferSize = 8192;
-					var buffer = new byte[bufferSize];
-					long totalBytes = response.Content.Headers.ContentLength ?? 0;
-					long downloadedBytes = 0;
-					int bytesRead;
+            using (var response = await client.GetAsync(url, HttpCompletionOption.ResponseHeadersRead))
+            {
+                response.EnsureSuccessStatusCode();
 
-					while ((bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length)) > 0)
-					{
-						await fileStream.WriteAsync(buffer, 0, bytesRead);
-						downloadedBytes += bytesRead;
-						Console.SetCursorPosition(0, Console.CursorTop);
-						Console.Write($"Downloading: {CalculateProgressPercentage(downloadedBytes, totalBytes)}%" + " (" + downloadedBytes + " / " + totalBytes + "Bytes");
-					}
-				}
-			}
+                using (var stream = await response.Content.ReadAsStreamAsync())
+                using (var fileStream = new FileStream(filePath, FileMode.Create, FileAccess.Write))
+                {
+                    const int bufferSize = 8192;
+                    var buffer = new byte[bufferSize];
+                    long totalBytes = response.Content.Headers.ContentLength ?? 0;
+                    long downloadedBytes = 0;
+                    int bytesRead;
 
-			Console.WriteLine();
-			Console.WriteLine($"File downloaded and saved to: {filePath}");
-		}
-	}
+                    while ((bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length)) > 0)
+                    {
+                        await fileStream.WriteAsync(buffer, 0, bytesRead);
+                        downloadedBytes += bytesRead;
+                        Console.SetCursorPosition(0, Console.CursorTop);
+                        Console.Write($"Downloading: {CalculateProgressPercentage(downloadedBytes, totalBytes)}%" + " (" + downloadedBytes + " / " + totalBytes + "Bytes");
+                    }
+                }
+            }
 
-	static void CopyFile (string sourcePath, string destinationDir)
-	{
-		string fileName = Path.GetFileName(sourcePath);
-		string destinationPath = string.IsNullOrEmpty(destinationDir)
-			? Path.Combine(Environment.CurrentDirectory, fileName)
-			: Path.Combine(destinationDir, fileName);
+            Console.WriteLine();
+            Console.WriteLine($"File downloaded and saved to: {filePath}");
 
-		File.Copy(sourcePath, destinationPath, true);
+            if (shouldExtract && IsZipFile(filePath))
+            {
+                ExtractZipFile(filePath, destination);
+                File.Delete(filePath);
+                Console.WriteLine($"Zip file extracted and deleted: {filePath}");
+            }
+        }
+    }
 
-		Console.WriteLine($"File copied to: {destinationPath}");
-	}
+    static void CopyFile(string sourcePath, string destinationDir)
+    {
+        string fileName = Path.GetFileName(sourcePath);
+        string destinationPath = string.IsNullOrEmpty(destinationDir)
+            ? Path.Combine(Environment.CurrentDirectory, fileName)
+            : Path.Combine(destinationDir, fileName);
 
-	static int CalculateProgressPercentage (long receivedBytes, long totalBytes)
-	{
-		if (totalBytes > 0)
-		{
-			double progress = (double)receivedBytes / totalBytes;
-			return (int)(progress * 100);
-		}
+        File.Copy(sourcePath, destinationPath, true);
 
-		return 0;
-	}
+        Console.WriteLine($"File copied to: {destinationPath}");
+
+        if (shouldExtract && IsZipFile(destinationPath))
+        {
+            ExtractZipFile(destinationPath, destinationDir);
+            File.Delete(destinationPath);
+            Console.WriteLine($"Zip file extracted and deleted: {destinationPath}");
+        }
+    }
+
+    static void ExtractZipFile(string zipFilePath, string destinationDir)
+    {
+        ZipFile.ExtractToDirectory(zipFilePath, destinationDir);
+    }
+
+    static int CalculateProgressPercentage(long receivedBytes, long totalBytes)
+    {
+        if (totalBytes > 0)
+        {
+            double progress = (double)receivedBytes / totalBytes;
+            return (int)(progress * 100);
+        }
+
+        return 0;
+    }
 }
