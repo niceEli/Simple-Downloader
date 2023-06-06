e09dc5263d1689decede604710afbb1a11e0b6fc
Eli <78963198+niceEli@users.noreply.github.com>
2023-06-06 17:28:33 -0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -2,7 +2,6 @@ using System;
 using System.IO;
 using System.Net.Http;
 using System.Threading.Tasks;
-using System.IO.Compression;
 
 class Program
 {
@@ -10,19 +9,18 @@ class Program
     {
         if (args.Length < 1)
         {
-            Console.WriteLine("Usage: Simple-Downloader <url> [<directory>] [-E or --Extract]");
+            Console.WriteLine("Usage: Simple-Downloader <url> [<directory>]");
             return;
         }
 
         string source = args[0];
         string destination = args.Length > 1 ? args[1] : "";
-        bool shouldExtract = args.Length > 2 && (args[2] == "-E" || args[2] == "--Extract");
 
         try
         {
             if (IsUrl(source))
             {
-                await DownloadFileAsync(source, destination, shouldExtract);
+                await DownloadFileAsync(source, destination);
             }
             else
             {
@@ -32,14 +30,7 @@ class Program
                     return;
                 }
 
-                if (shouldExtract && IsZipFile(source))
-                {
-                    ExtractZipFile(source, destination);
-                }
-                else
-                {
-                    CopyFile(source, destination, shouldExtract);
-                }
+                CopyFile(source, destination);
             }
         }
         catch (Exception ex)
@@ -53,37 +44,87 @@ class Program
         return Uri.TryCreate(path, UriKind.Absolute, out _);
     }
 
-    static bool IsZipFile(string path)
+    static async Task DownloadFileAsync(string url, string destination)
     {
-        string extension = Path.GetExtension(path);
-        return string.Equals(extension, ".zip", StringComparison.OrdinalIgnoreCase);
-    }
-
-    static async Task DownloadFileAsync(string url, string destination, bool shouldExtract)
-    {
-        // Rest of the code
-
-        Console.WriteLine($"File downloaded and saved to: {filePath}");
-
-        if (shouldExtract && IsZipFile(filePath))
+        using (HttpClient client = new HttpClient())
         {
-            ExtractZipFile(filePath, destination);
-            File.Delete(filePath);
-            Console.WriteLine($"Zip file extracted and deleted: {filePath}");
+            Uri uri = new Uri(url);
+            string fileName = Path.GetFileName(uri.LocalPath);
+            string filePath = string.IsNullOrEmpty(destination)
+                ? Path.Combine(Environment.CurrentDirectory, fileName)
+                : Path.Combine(destination, fileName);
+
+            using (var response = await client.GetAsync(url, HttpCompletionOption.ResponseHeadersRead))
+            {
+                response.EnsureSuccessStatusCode();
+
+                using (var stream = await response.Content.ReadAsStreamAsync())
+                using (var fileStream = new FileStream(filePath, FileMode.Create, FileAccess.Write))
+                {
+                    const int bufferSize = 8192;
+                    var buffer = new byte[bufferSize];
+                    long totalBytes = response.Content.Headers.ContentLength ?? 0;
+                    long downloadedBytes = 0;
+                    int bytesRead;
+
+                    while ((bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length)) > 0)
+                    {
+                        await fileStream.WriteAsync(buffer, 0, bytesRead);
+                        downloadedBytes += bytesRead;
+                        Console.SetCursorPosition(0, Console.CursorTop);
+                        Console.Write($"Downloading: {CalculateProgressPercentage(downloadedBytes, totalBytes)}%");
+
+                        DrawProgressBar(downloadedBytes, totalBytes);
+                    }
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine($"File downloaded and saved to: {filePath}");
         }
     }
 
-    static void CopyFile(string sourcePath, string destinationDir, bool shouldExtract)
+    static void CopyFile(string sourcePath, string destinationDir)
     {
-        // Rest of the code
+        string fileName = Path.GetFileName(sourcePath);
+        string destinationPath = string.IsNullOrEmpty(destinationDir)
+            ? Path.Combine(Environment.CurrentDirectory, fileName)
+            : Path.Combine(destinationDir, fileName);
 
-        if (shouldExtract && IsZipFile(destinationPath))
-        {
-            ExtractZipFile(destinationPath, destinationDir);
-            File.Delete(destinationPath);
-            Console.WriteLine($"Zip file extracted and deleted: {destinationPath}");
-        }
+        File.Copy(sourcePath, destinationPath, true);
+
+        Console.WriteLine($"File copied to: {destinationPath}");
     }
 
-    // Rest of the code
+    static int CalculateProgressPercentage(long receivedBytes, long totalBytes)
+    {
+        if (totalBytes > 0)
+        {
+            double progress = (double)receivedBytes / totalBytes;
+            return (int)(progress * 100);
+        }
+
+        return 0;
+    }
+
+    static void DrawProgressBar(long completed, long total)
+    {
+        const int ProgressBarWidth = 40;
+        const string ProgressBarChars = "░▒▓█";
+
+        double progress = (double)completed / total;
+        int completedWidth = (int)(progress * ProgressBarWidth);
+        int remainingWidth = ProgressBarWidth - completedWidth;
+
+        int numFullChars = completedWidth / (ProgressBarWidth / ProgressBarChars.Length);
+        int numPartialChar = completedWidth % (ProgressBarWidth / ProgressBarChars.Length);
+
+        string progressBar = new string(ProgressBarChars[ProgressBarChars.Length - 1], numFullChars);
+        if (numPartialChar > 0)
+            progressBar += ProgressBarChars[numPartialChar - 1];
+
+        progressBar += new string(ProgressBarChars[0], remainingWidth);
+
+        Console.Write($" {progressBar}");
+    }
 }
